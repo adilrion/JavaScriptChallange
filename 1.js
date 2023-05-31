3c9d2add7161683275fe9dd98d420a79bb6c4291
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-06-01 01:13:00 +0600

    day 1 problem 1

---
+++ 1.js
@@ -0,0 +1,33 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
+
+
+function sum(nums, target) {
+    const map = new Map()
+    for(let i = 0; i<nums.length; i++){
+        const complements = target - nums[i];
+
+        if(map.has(complements)){
+            return [map.get(complements), i];
+        }
+
+        map.set(nums[i], i);
+
+    }
+    return [];
+  }
+  
+  
+
+const  nums = [2,7,11,15];
+const target = 9;
+// sum(nums, target)
+console.log(sum(nums, target));
+
+
+
+
